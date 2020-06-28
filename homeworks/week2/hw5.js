dc97940910ae5beeb40eda71852cff19e3e37d24
Laila <orange80416@gmail.com>
2020-06-28 22:02:26 +0800

    first-draft

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,19 @@
 function join(arr, concatStr) {
-  
+    let ans =''
+    for (let i=0; i<(arr.length-1); i++){
+        ans += arr[i] + concatStr
+    }
+    ans += arr[arr.length-1]//最後一個要分開加，不然會多一個concatStr
+    //console.log('ans:',ans)
+    return ans
 }
 
 function repeat(str, times) {
-  
+    let ans = ''
+    for (let i=0 ; i<times ; i++){
+        ans += str
+    }
+    return ans  
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+//console.log(join(["aaa", "bb", "c", "dddd"], ',,'))
